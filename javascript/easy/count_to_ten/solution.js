10270a4cbd8121cc204d94fbaf8b3993368a9f87
Jesse Kroon <j.kroon@vistacollege.nl>
2024-09-26 16:49:09 +0200

    add while loop

--- javascript/easy/count_to_ten/solution.js
+++ javascript/easy/count_to_ten/solution.js
@@ -2,6 +2,14 @@
 // Schrijf een programma die de getallen 1 t/m 10 naar de console print
 // Gebruik hierbij een loop
 
+// FOR LOOP
 for (let i = 1; i <= 10; i++) {
   console.log(i);
+}
+
+// WHILE LOOP
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
 }
