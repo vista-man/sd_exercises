364e890603d22eb41cc4b7d9a769ce99b2779f65
Jesse Kroon <j.kroon@vistacollege.nl>
2024-09-27 10:47:43 +0200

    add csharp stuff

---
+++ csharp/easy/count_to_ten/Program.cs
@@ -0,0 +1,5 @@
+﻿// OPDRACHT
+// Schrijf een programma die de getallen 1 t/m 10 naar de console print
+// Gebruik hierbij een loop
+
+Console.WriteLine("VERWIJDER DEZE REGEL");
---
+++ csharp/solutions/easy/count_to_ten/solution.cs
@@ -0,0 +1,15 @@
+// OPDRACHT
+// Schrijf een programma die de getallen 1 t/m 10 naar de console print
+// Gebruik hierbij een loop
+
+// FOR LOOP
+for (int i = 0; i <= 10; i++) {
+    Console.WriteLine(i);
+}
+
+// WHILE LOOP
+int i = 1;
+while (i <= 10) {
+    Console.WriteLine(i);
+    i++;
+}
