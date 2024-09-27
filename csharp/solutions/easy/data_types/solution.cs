ef743e135cdf617b6e09b90e8ac61cfb6b3275a1
Jesse Kroon <j.kroon@vistacollege.nl>
2024-09-27 12:31:51 +0200

    add data types for csharp

---
+++ csharp/solutions/easy/data_types/solution.cs
@@ -0,0 +1,11 @@
+// OPDRACHT
+// Declareer enkele variabelen van verschillende types.
+
+// Declareer onder deze regel een variabel genaamd myName met het datatype String
+string myName = "Dev Eloper";
+
+// Declareer onder deze regel een variabel genaamd myAge met het datatype Number
+int myAge = 21;
+
+// Declareer onder deze regel een variabel genaamd isAdult met het datatype Boolean
+bool isAdult = true;
