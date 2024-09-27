d0bd8782f9433cf5505b72d9d895c07cc0c87bfa
Jesse Kroon <j.kroon@vistacollege.nl>
2024-09-27 12:24:45 +0200

    add print your name for csharp

---
+++ csharp/solutions/easy/print_your_name/solution.cs
@@ -0,0 +1,15 @@
+// OPDRACHT:
+// Print jouw naam naar de console
+// In de console verwacht ik de volgende tekst: Hallo, mijn naam is <JOUW_NAAM>.
+
+// MOGELIJKE OPLOSSINGEN
+
+// String literal
+Console.WriteLine("Hallo, mijn naam is Dev.");
+
+// Variable
+string myName = "Dev";
+Console.WriteLine("Hallo, mijn naam is " + myName + ".");
+
+// Template string
+Console.WriteLine($"Hallo, mijn naam is {myName}.");
