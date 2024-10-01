5df92d21174e85d41729c939a532409814c810fc
vista-man <524715@vistacollege.nl>
2024-10-01 14:05:16 +0200

    Update exercise.py

--- python/easy/data_types/exercise.py
+++ python/easy/data_types/exercise.py
@@ -2,9 +2,11 @@
 OPDRACHT
 Declareer enkele variabelen van verschillende types.
 '''
+import time
 
 # Declareer onder deze regel een variabel genaamd myName met het datatype String
-
+myName = "Alvin"
 # Declareer onder deze regel een variabel genaamd myAge met het datatype Number
-
+myAge = 16
 # Declareer onder deze regel een variabel genaamd isAdult met het datatype Boolean
+isAdult = False
