ddad07e4c62d4a58e624864f4eeb3c1d3ec833f5
vista-man <524715@vistacollege.nl>
2024-10-01 13:56:55 +0200

    Update exercise.py

--- python/easy/count_to_ten/exercise.py
+++ python/easy/count_to_ten/exercise.py
@@ -3,3 +3,15 @@ OPDRACHT
 Schrijf een programma die de getallen 1 t/m 10 naar de console print
 Gebruik hierbij een loop
 '''
+
+import time
+
+i = 1
+
+while i < 11:
+    print(i)
+    i += 1
+else:
+    print("done")
+
+time.sleep(1)
